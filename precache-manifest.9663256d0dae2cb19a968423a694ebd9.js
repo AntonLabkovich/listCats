818b2baeb2b2ceb20f905c5365d7c712635be25d
AntonLabkovich <sharcnatch@gmail.com>
2020-07-22 17:45:20 +0300

    Updates

---
+++ precache-manifest.9663256d0dae2cb19a968423a694ebd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c35fdd13c1ec3e055631cda510728960",
+    "url": "/listCats/index.html"
+  },
+  {
+    "revision": "a279e9fbe92bd36cd931",
+    "url": "/listCats/static/css/main.f53ad307.chunk.css"
+  },
+  {
+    "revision": "961b3eae78ad7a304e7b",
+    "url": "/listCats/static/js/2.42f4c6a3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/listCats/static/js/2.42f4c6a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a279e9fbe92bd36cd931",
+    "url": "/listCats/static/js/main.02a63753.chunk.js"
+  },
+  {
+    "revision": "469550123bce6c6ea9a3",
+    "url": "/listCats/static/js/runtime-main.2b680336.js"
+  }
+]);
